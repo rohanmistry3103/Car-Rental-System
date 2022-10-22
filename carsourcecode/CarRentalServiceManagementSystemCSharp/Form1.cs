bb6b7fbc1ca7342cb626ee04e631cb7a2de2487f
Rohan Mistry <85560853+rohanmistry3103@users.noreply.github.com>
2022-10-22 14:01:54 +0530

    Add files via upload

---
+++ carsourcecode/CarRentalServiceManagementSystemCSharp/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarRentalServiceManagementSystemCSharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "rohan3103" || textBox2.Text == "3103")
+            {
+                MessageBox.Show("You are logged in successfully..");
+                this.Visible = false;
+                Home obj1 = new Home();
+                obj1.ShowDialog();
+                textBox1.Text = "";
+                textBox2.Text = "";
+                
+            }
+            else
+            {
+                MessageBox.Show("Invalid Username Or Password.");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
